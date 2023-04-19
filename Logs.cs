24961084df0463f3810de0586f516f1feb0af125
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-19 13:31:13 +0300

    Create Logs.cs

---
+++ Logs.cs
@@ -0,0 +1,23 @@
+public class Users
+{
+  public void Create(User u)
+  {
+      
+  }
+  public User Read(User u)
+  {
+      
+  }
+  public void Update(User u)
+  {
+      
+  }
+  public void Delete(User u)
+  {
+      
+  }
+  public List<User> List()
+  {
+      
+  }
+}
