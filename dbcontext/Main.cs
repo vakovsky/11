22a372e433bc305ed49d163d3148c7ee3af1f627
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-20 21:46:57 +0300

    Update Main.cs

--- dbcontext/Main.cs
+++ dbcontext/Main.cs
@@ -19,7 +19,7 @@
     }
     public class DataBaseContext : DbContext
     {
-        public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\mitko\Documents\databaseCF.mdf;Integrated Security=True;Connect Timeout=30") { }
+        public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User3\Documents\databaseCF.mdf;Integrated Security=True;Connect Timeout=30") { }
         public DbSet<User> Users { get; set; }
         public DbSet<Log> Logs { get; set; }
     }
