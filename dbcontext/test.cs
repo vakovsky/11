b8b5f2b1bfd1b770d585ce65dafa8c640ba874de
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-20 16:12:38 +0300

    Update test.cs

--- dbcontext/test.cs
+++ dbcontext/test.cs
@@ -1,1 +1,37 @@
-1
+public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public virtual List<Log> Logs { get; set; }
+    }
+    public class Log
+    {
+        public int LogId { get; set; }
+        public string Info { get; set; }
+        public DateTime Created { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User3\Documents\databaseCF.mdf;Integrated Security=True;Connect Timeout=30") { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Log> Logs { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataBaseContext context = new DataBaseContext())
+            {
+                User user = new User();
+                Console.Write("Username:");
+                user.Username = Console.ReadLine();
+                Console.Write("Password:");
+                user.Password = Console.ReadLine();
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+        }
+    }
