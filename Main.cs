d267246cda4337a4190aba1e186cb9a04cf52817
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-20 13:22:28 +0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,21 @@
+  Users users = new Users();
+  User user = new User();
+
+  //user.Username = Console.ReadLine();
+  //Console.Write("Password:");
+  //user.Password = Console.ReadLine();
+  //users.Create(user);
+
+  //Console.Write("UserId:");
+  //user.UserId = int.Parse(Console.ReadLine());
+  //users.Delete(user);
+
+  //user.UserId = int.Parse(Console.ReadLine());
+  //user.Username = "user36";
+  //user.Password = "pass36";
+  //users.Update(user);
+
+  //user.UserId = int.Parse(Console.ReadLine());
+  //user = users.Read(user);
+  //Console.WriteLine(user.Username);
+  //Console.WriteLine(user.Password);
