ebb0216923016993c183b9dbfb2ce121c2d0ad0a
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-20 20:38:38 +0300

    Update Main.cs

--- dbcontext/Main.cs
+++ dbcontext/Main.cs
@@ -10,65 +10,103 @@ public class User
 }
 public class Log
 {
-    public int LogId { get; set; }
-    public string Info { get; set; }
-    public DateTime Created { get; set; }
-    public int UserId { get; set; }
-    public virtual User User { get; set; }
+        public int LogId { get; set; }
+        public string Info { get; set; }
+        public DateTime Created { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
 }
 public class DataBaseContext : DbContext
 {
-    public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User3\Documents\databaseCF.mdf;Integrated Security=True;Connect Timeout=30") { }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Log> Logs { get; set; }
+        public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User3\Documents\databaseCF.mdf;Integrated Security=True;Connect Timeout=30") { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Log> Logs { get; set; }
 }
+
+class Users
+{
+        public void Create(User u)
+        {
+                using (DataBaseContext context = new DataBaseContext())
+                {
+                    User user = new User();
+                    Console.Write("Username:");
+                    user.Username = Console.ReadLine();
+                    Console.Write("Password:");
+                    user.Password = Console.ReadLine();
+                    context.Users.Add(user);
+                    context.SaveChanges();
+                }
+        }
+
+        public User Read(User u)
+        {
+                using (DataBaseContext context = new DataBaseContext())
+                {
+                    Console.Write("UserId:");
+                    int id = int.Parse(Console.ReadLine());
+                    var user = (from u in context.Users
+                                where u.UserId == id
+                                select u).Single();
+                    Console.WriteLine(user.Username);
+                    Console.WriteLine(user.Password);
+                }
+        }
+
+        public void Update(User u)
+        {
+                using (DataBaseContext context = new DataBaseContext())
+                {
+                    Console.Write("UserId:");
+                    int id = int.Parse(Console.ReadLine());
+                    User user = (from u in context.Users
+                                where u.UserId == id
+                                select u).Single();
+                    user.Username = "user36";
+                    user.Password = "pass36";
+                    context.SaveChanges();
+                }
+        }
+
+        public void Delete(User u)
+        {
+                using (DataBaseContext context = new DataBaseContext())
+                {
+                    Console.Write("UserId:");
+                    int id = int.Parse(Console.ReadLine());
+                    User user = (from u in context.Users
+                                where u.UserId == id
+                                select u).Single();
+                    context.Users.Remove(user);
+                    context.SaveChanges();
+                }
+        }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        using (DataBaseContext context = new DataBaseContext())
-        {
-            User user = new User();
-            Console.Write("Username:");
-            user.Username = Console.ReadLine();
-            Console.Write("Password:");
-            user.Password = Console.ReadLine();
-            context.Users.Add(user);
-            context.SaveChanges();
-        }
+        Console.Write("Username:");
+        user.Username = Console.ReadLine();
+        Console.Write("Password:");
+        user.Password = Console.ReadLine();
+        users.Create(user);
 
-        /*using (DataBaseContext context = new DataBaseContext())
-        {
-            Console.Write("UserId:");
-            int id = int.Parse(Console.ReadLine());
-            var user = (from u in context.Users
-                        where u.UserId == id
-                        select u).Single();
-            Console.WriteLine(user.Username);
-            Console.WriteLine(user.Password);
-        }*/
+        Console.Write("UserId:");
+        user.UserId = int.Parse(Console.ReadLine());
+        users.Delete(user);
 
-        /*using (DataBaseContext context = new DataBaseContext())
-        {
-            Console.Write("UserId:");
-            int id = int.Parse(Console.ReadLine());
-            User user = (from u in context.Users
-                        where u.UserId == id
-                        select u).Single();
-            user.Username = "user36";
-            user.Password = "pass36";
-            context.SaveChanges();
-        }*/
+        Console.Write("UserId:");
+        user.UserId = int.Parse(Console.ReadLine());
+        user.Username = "user36";
+        user.Password = "pass36";
+        users.Update(user);
 
-        /*using (DataBaseContext context = new DataBaseContext())
-        {
-            Console.Write("UserId:");
-            int id = int.Parse(Console.ReadLine());
-            User user = (from u in context.Users
-                        where u.UserId == id
-                        select u).Single();
-            context.Users.Remove(user);
-            context.SaveChanges();
-        }*/
+        Console.Write("UserId:");
+        user.UserId = int.Parse(Console.ReadLine());
+        user = users.Read(user);
+        Console.WriteLine(user.Username);
+        Console.WriteLine(user.Password);
     }
 }
