4b05510143156de0e3e4fae60a2918b4e14dcac5
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-24 16:27:28 +0300

    Update Users.sql

--- access/Users.sql
+++ access/Users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [Users] (
     [UserId]   AUTOINCREMENT,
-    [Username] VARCHAR (100)  NOT NULL,
-    [Password] VARCHAR (100) NULL,
-    CONSTRAINT UserId_PK PRIMARY KEY ([UserId]),
-    UNIQUE ([Username])
+    [Username] TEXT (100)  NOT NULL,
+    [Password] TEXT (100) NULL,
+    CONSTRAINT UserId PRIMARY KEY ([UserId]),
+    UNIQUE (Username)
 )
