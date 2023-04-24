8de22514a011be6dd135401627f894daceedaf59
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-24 14:52:45 +0300

    Create User.sql

---
+++ access/User.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [Users] (
+    [UserId]   AUTOINCREMENT,
+    [Username] VARCHAR (100)  NOT NULL,
+    [Password] VARCHAR (100) NULL,
+    CONSTRAINT UserId_PK PRIMARY KEY ([UserId]),
+    UNIQUE (Username)
+)
