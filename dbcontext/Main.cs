d6a05a535298f2b7b4f45f5d5ef1f3c505268bcc
Dimiter Vakovsky <vakovsky@gmail.com>
2023-04-24 16:42:13 +0300

    Update Main.cs

--- dbcontext/Main.cs
+++ dbcontext/Main.cs
@@ -1,92 +1,3 @@
-
-    public class User
-    {
-        [Key]
-        public int UserId { get; set; }
-        [Index(IsUnique = true), Required]
-        [StringLength(200)]
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public virtual List<Log> Logs { get; set; }
-    }
-    public class Log
-    {
-        public int LogId { get; set; }
-        public string Info { get; set; }
-        public DateTime Created { get; set; }
-        public int UserId { get; set; }
-        public virtual User User { get; set; }
-    }
-    public class DataBaseContext : DbContext
-    {
-        public DataBaseContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User3\Documents\databaseCF.mdf;Integrated Security=True;Connect Timeout=30") { }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Log> Logs { get; set; }
-    }
-
-    class Users
-    {
-        public void Create(User u)
-        {
-            using (DataBaseContext context = new DataBaseContext())
-            {
-                context.Users.Add(u);
-                context.SaveChanges();
-            }
-        }
-
-        public User Read(User u)
-        {
-            using (DataBaseContext context = new DataBaseContext())
-            {
-                u = (from user in context.Users
-                     where user.UserId == u.UserId
-                     select user).Single();
-                return u;
-            }
-        }
-
-        public void Update(User u)
-        {
-            using (DataBaseContext context = new DataBaseContext())
-            {
-                User user = (from userold in context.Users
-                             where userold.UserId == u.UserId
-                             select userold).Single();
-                user.Username = u.Username;
-                user.Password = u.Password;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(User u)
-        {
-            using (DataBaseContext context = new DataBaseContext())
-            {
-                User user = (from userold in context.Users
-                             where userold.UserId == u.UserId
-                             select userold).Single();
-                context.Users.Remove(user);
-                context.SaveChanges();
-            }
-        }
-        public List<User> List()
-        {
-            List<User> users = null;
-            using (var context = new DataBaseContext())
-            {
-                users = (from u in context.Users
-                         orderby u.UserId
-                         select u).ToList();
-            }
-            return users;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
             Users users = new Users();
             User user = new User();
 
@@ -118,9 +29,6 @@
             {
                 Console.WriteLine("{0} {1}", user1.UserId, user1.Username);
             }*/
-        }
-    }
-
 
 //using System.ComponentModel.DataAnnotations;
 //using System.ComponentModel.DataAnnotations.Schema;
