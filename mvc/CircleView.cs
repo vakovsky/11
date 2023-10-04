64741ed0c180979e607ceec67f6a784a7eee69f3
Dimiter Vakovsky <vakovsky@gmail.com>
2023-10-04 13:44:31 +0300

    view1

---
+++ mvc/CircleView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CircleView
+    {
+        public int R { get; set; }
+        public double Area { get; set; }
+        //
+        public void Read()
+        {
+            Console.Write("r = ");
+            R = int.Parse(Console.ReadLine());
+        }
+        //
+        public void Write()
+        {
+            Console.WriteLine("r = {0}, s = {1} ", R, Area);
+        }       
+    }
+}
---
+++ mvc/RectView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class RectView
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public double Area { get; set; }
+        //
+        public void Read()
+        {
+            Console.Write("a = ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            B = int.Parse(Console.ReadLine());
+        }
+        //
+        public void Write()
+        {
+            Console.WriteLine("a = {0}, b = {1}, s = {2} ", A, B, Area);
+        }       
+    }
+}
