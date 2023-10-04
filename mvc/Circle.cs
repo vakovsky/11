9a674cea65a9ee4d6fb510e4bb7af6b060335416
Dimiter Vakovsky <vakovsky@gmail.com>
2023-10-04 13:28:47 +0300

    Update Circle.cs

---
+++ mvc/Circle.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Circle
+    {
+        public int R { get; set; }
+        public double Area
+        {
+            get
+            {
+                return Math.PI * R * R;
+            }
+        }
+    }
+}
