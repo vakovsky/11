7fec3388dbcf319ced9c86fcf4678e1b563976ec
Dimiter Vakovsky <vakovsky@gmail.com>
2023-10-12 12:51:22 +0300

    Update Circle.cs

--- mvc/workshop/Circle.cs
+++ mvc/workshop/Circle.cs
@@ -1,1 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ClassLibrary1
+{
+    public class Circle
+    {
+        public int Id { get; set; }
+        public int R { get; set; }
+        public double Area
+        {
+            get
+            {
+                return Math.PI * R * R;
+            }
+        }
+    }
+}
