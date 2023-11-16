a345fd746e94ee3f14bbe123b1db46ffc0656cde
Dimiter Vakovsky <vakovsky@gmail.com>
2023-11-16 15:14:21 +0200

    Update and rename test.txt to test.cs

---
+++ mvc/workshop/test.cs
@@ -0,0 +1,51 @@
+        static string ComputeMD5Hash(string rawData)
+        {
+            // Create a MD5
+            using (MD5 md5 = MD5.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+        static string ComputeSha512Hash(string rawData)
+        {
+            // Create a SHA512
+            using (SHA512 sha512Hash = SHA512.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha512Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
