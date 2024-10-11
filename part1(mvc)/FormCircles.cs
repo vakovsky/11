41a107eea081150b4928ce1fb994ec9286a35f86
Dimiter Vakovsky <vakovsky@gmail.com>
2024-10-11 12:21:45 +0300

    1

---
+++ part1(mvc)/FormCircles.cs
@@ -0,0 +1,64 @@
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormCircles : Form
+    {
+        public FormCircles()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            Circle circle = new Circle();
+            FormCircleView formCircleView = new FormCircleView();
+            formCircleView.ShowDialog();
+            circle.R = int.Parse(formCircleView.textBoxR.Text);
+            listBox1.Items.Add(circle);
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            Circle circle = listBox1.Items[index] as Circle;
+            FormCircleView formCircleView = new FormCircleView();
+            formCircleView.textBoxR.Text = circle.R.ToString();
+            formCircleView.textBoxArea.Text = circle.Area.ToString();
+            formCircleView.ShowDialog();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            Circle circle = listBox1.Items[index] as Circle;
+            FormCircleView formCircleView = new FormCircleView();
+            formCircleView.textBoxR.Text = circle.R.ToString();
+            formCircleView.textBoxArea.Text = circle.Area.ToString();
+            formCircleView.ShowDialog();
+            circle.R = int.Parse(formCircleView.textBoxR.Text);
+            listBox1.Items[index] = circle;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            Circle circle = listBox1.Items[index] as Circle;
+            FormCircleView formCircleView = new FormCircleView();
+            formCircleView.textBoxR.Text = circle.R.ToString();
+            formCircleView.textBoxArea.Text = circle.Area.ToString();
+            formCircleView.ShowDialog();
+            listBox1.Items.RemoveAt(index);
+        }
+    }
+}
