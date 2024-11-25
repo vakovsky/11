75ded6fc2812063821ab66481984a165d0bf037f
Dimiter Vakovsky <vakovsky@gmail.com>
2024-11-25 09:07:44 +0200

    Update Circle.cs

--- part1(mvc)/Circle.cs
+++ part1(mvc)/Circle.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Circle
+    public int ID { get; set; }
+    public int R { get; set; }
+    public double Area
     {
-        public int ID { get; set; }
-        public int R { get; set; }
-        public double Area
+        get
         {
-            get
-            {
-                return Math.PI * Math.Pow(R, 2);
-            }
+            return Math.PI * Math.Pow(R, 2);
         }
-        public override string ToString()
+    }
+    public double Perimeter
+    {
+        get
         {
-             return ID.ToString() + " " +
-                 R.ToString() + " Area(" + Area.ToString("F2") +")";
+            return 2 * Math.PI * R;
         }
+    }
+    public override string ToString()
+    {
+        return ID.ToString() + " " +
+            R.ToString() + " Area(" + Area.ToString("F2") +")"
+            + " Perimeter(" + Perimeter.ToString("F2") + ")";
     }
 }
