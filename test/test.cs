664e0cf388f6fc0e96a4eeeae08b5fd56a24c018
Dimiter Vakovsky <vakovsky@gmail.com>
2025-04-17 12:33:34 +0300

    Update test.cs

--- test/test.cs
+++ test/test.cs
@@ -1,1 +1,16 @@
-hello
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
