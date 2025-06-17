7cc28f1b61acb90638c791b205e22ea2768e36ad
Dimiter Vakovsky <vakovsky@gmail.com>
2025-06-17 17:36:49 +0300

    Add files via upload

---
+++ test/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Circle
+    {
+        public int ID { get; set; }
+        public int R { get; set; }
+        public double Area
+        {
+            get
+            {
+                return Math.PI * Math.Pow(R, 2);
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return 2 * Math.PI * R;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} R({1}) Area({2}) Perimeter({3})",
+                ID.ToString(),
+                R.ToString(),
+                Area.ToString("F2"),
+                Perimeter.ToString("F2"));
+        }
+    }
+}
